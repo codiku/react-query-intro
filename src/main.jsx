92afbf4e1a746114f75c8aa015892e698d94d761
rlebhar-clone <robin.lebhar@getclone.io>
2024-07-21 20:35:47 +0200

    setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,18 @@ import "./index.css";
 import { App } from "./App";
 import { ChakraProvider } from '@chakra-ui/react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 const queryClient = new QueryClient()
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <QueryClientProvider client={queryClient}>
         <ChakraProvider>
             <App />
-            <ReactQueryDevtools initialIsOpen={false} />
         </ChakraProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 );
+
+
+
