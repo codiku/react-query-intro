26b274621e1e81ada65917c96ed5a22dac3e70f7
rlebhar-clone <robin.lebhar@getclone.io>
2024-07-29 16:17:56 +0200

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,39 @@
 import { Box, Text } from "@chakra-ui/react";
-import { PokemonAPI } from "../api/pokemon-api";
-PokemonAPI.fetchPokemons(1, 5)
+import { useState } from "react";
+import { PokemonAPI } from "../api/pokemon-api"
+import { useEffect } from "react";
 export const Home = () => {
+  const [pokemonList, setPokemonList] = useState([])
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  async function loadPokemons() {
+    try {
+      setIsLoading(true)
+      const pokemonResponse = await PokemonAPI.fetchPokemons(1, 151)
+      setPokemonList(pokemonResponse)
+
+    } catch (err) {
+      setErrorMessage(err.message)
+
+    } finally {
+      setIsLoading(false)
+    }
+  }
+  useEffect(() => {
+    loadPokemons()
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (errorMessage) {
+    return <div>Oups : {errorMessage}</div>
+  }
   return (
 
     <Box>
-
+      {JSON.stringify(pokemonList)}
     </Box>
 
   );
