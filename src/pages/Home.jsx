45ab8d3ec41572fc7fc8f6ae1691bc0a58b5f3aa
rlebhar-clone <robin.lebhar@getclone.io>
2024-08-02 15:23:42 +0200

    wip

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,50 @@
-import { List, ListItem, Container, Image } from "@chakra-ui/react";
+import {
+  List,
+  ListItem,
+  Container,
+  Image,
+  HStack,
+  Button,
+  Skeleton,
+} from "@chakra-ui/react";
 import { PokemonAPI } from "../api/pokemon-api";
 import { useQuery } from "@tanstack/react-query";
 import { ROUTES } from "../router";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+
 const PER_PAGE = 5;
+const MAX_PAGE = 20;
 
 export const Home = () => {
   const [page, setPage] = useState(1);
   const navigate = useNavigate();
   const {
-    data: pokemonList,
+    data: pokemonList = [],
     error,
     isLoading,
   } = useQuery({
     queryKey: ["pokemons", "page-" + page],
     queryFn: () => PokemonAPI.fetchPokemons(page, PER_PAGE),
+    staleTime: Infinity,
   });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const loadNextPage = async () => {
+    if (page < MAX_PAGE) {
+      setPage((page) => page + 1);
+    }
+  };
+
+  const loadPreviousPage = async () => {
+    if (page > 1) {
+      setPage((page) => page - 1);
+    }
+  };
 
   if (error) {
     return <div>Oups : {error.message}</div>;
   }
+  console.log(pokemonList, isLoading);
   return (
     <Container mt={10}>
       <List spacing={3} w="100%">
@@ -47,7 +67,21 @@ export const Home = () => {
             {pokemon.name}
           </ListItem>
         ))}
+        {isLoading &&
+          Array.from({ length: PER_PAGE }).map((_, i) => (
+            <ListItem key={"skeleton" + i}>
+              <Skeleton height={"68px"} />
+            </ListItem>
+          ))}
       </List>
+      <HStack spacing={4} display={"flex"} justifyContent={"center"} mt={10}>
+        <Button onClick={loadPreviousPage} isDisabled={page === 1}>
+          Load previous page
+        </Button>
+        <Button onClick={loadNextPage} isDisabled={page == MAX_PAGE}>
+          Load next page
+        </Button>
+      </HStack>
     </Container>
   );
 };
